f14ccb136fe55136ee7651650e0606f04dab4c54
Jenny Larsson <jenny.larsson@trustly.com>
2023-07-06 11:06:15 +0200

    Update source code and README for v0.0.6

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# TrustlyReactNativeWebView
-WebView instance to be used for React Native integration with Trustly Checkout
+# Trustly's ReactNative WebView Package
+
+This package is meant to be integrated into React Native implementations of Trustly's Checkout. The package allows communication between Trustly's Checkout application and React Native's WebView through the `postMessage` method.
+
+# Content
+
+- [Trustly's ReactNative WebView Package](#trustlys-reactnative-webview-package)
+- [Content](#content)
+  - [Installation](#installation)
+  - [Usage](#usage)
+
+## Installation
+
+```
+npm i trustly-react-native-webview
+```
+
+## Usage
+
+The Trustly React Native package exports a `WebView` component that can take three properties:
+
+- `uri`: The Checkout URL that is returned from [Trustly's API](https://eu.developers.trustly.com/doc/reference/api-protocol)
+- `onSuccess`: Optional function to be run as a callback when the order is done.
+- `onError`: Optional function to be run as a callback if the order encounter any issues or if the order is aborted by the user.
+
+The WebView component does not need to be modified and will start responding to events once added to your repository.
+
+Example:
+
+```
+import TrustlyWebView from 'trustly-react-native-webview';
+
+...
+
+<TrustlyWebView uri={uri} onSuccess={onSuccess} onError={onError}/>
+```
--- TrustlyWebView.js
+++ TrustlyWebView.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Linking } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React from "react";
+import { Linking } from "react-native";
+import { WebView } from "react-native-webview";
 
 export const NativeEventTypes = {
-  SUCCESS: 'onTrustlyCheckoutSuccess',
-  ERROR: 'onTrustlyCheckoutError',
-  REDIRECT: 'onTrustlyCheckoutRedirect',
-  ABORT: 'onTrustlyCheckoutAbort'
+  SUCCESS: "onTrustlyCheckoutSuccess",
+  ERROR: "onTrustlyCheckoutError",
+  REDIRECT: "onTrustlyCheckoutRedirect",
+  ABORT: "onTrustlyCheckoutAbort",
 };
-  
-export const trustlyApplicationName = 'TrustlyReactNativeWebView/v1';
-  
+
+export const trustlyApplicationName = "TrustlyReactNativeWebView/v1";
+
 export const trustlyCustomBridge = `
   window.addEventListener('message', (e) => {
     try {
       const data = e.data;
       const message = typeof data !== 'string' ? JSON.stringify(data) : data;
-
+  
       if (window.ReactNativeWebView) {
         window.ReactNativeWebView.postMessage(message, '*');
       }
@@ -32,11 +32,11 @@ export const trustlyCustomBridge = `
   true;
 `;
 
-const onMessage = (data, onSuccess, onError, onAbort) => {
+const onMessage = (data, onSuccess, onError) => {
   try {
     const { type, url, eventType } = JSON.parse(data);
 
-    const isCheckoutEvent = eventType === 'checkout-event';
+    const isCheckoutEvent = eventType === "checkout-event";
 
     if (isCheckoutEvent) {
       if (type === NativeEventTypes.REDIRECT) {
@@ -48,26 +48,23 @@ const onMessage = (data, onSuccess, onError, onAbort) => {
         }
       }
 
-      if (type === NativeEventTypes.ERROR) {
+      if (type === NativeEventTypes.ABORT || type === NativeEventTypes.ERROR) {
         onError();
       }
 
-      if (type === NativeEventTypes.ABORT) {
-        onAbort();
-      }
       if (type === NativeEventTypes.SUCCESS) {
         onSuccess();
       }
     }
   } catch (e) {
-    const error = typeof error === 'object' ? JSON.stringify(e) : e;
+    const error = typeof error === "object" ? JSON.stringify(e) : e;
     console.error(
       `Something unexpected happen when trying to map Trustly Checkout events. Error: ${error}`
     );
   }
 };
 
-const TrustlyWebView = ({ uri, onSuccess, onError, onAbort }) => {
+const TrustlyWebView = ({ uri, onSuccess, onError }) => {
   return (
     <WebView
       source={{ uri }}
@@ -76,7 +73,7 @@ const TrustlyWebView = ({ uri, onSuccess, onError, onAbort }) => {
       applicationNameForUserAgent={trustlyApplicationName}
       onMessage={({ nativeEvent }) => {
         try {
-          onMessage(nativeEvent.data, onSuccess, onError, onAbort);
+          onMessage(nativeEvent.data, onSuccess, onError);
         } catch (e) {
           console.error(
             `Format of nativeEvent.data does not match Trustly API. Data sent: ${nativeEvent.data}`
